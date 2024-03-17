3f598454d75e742122f44f6d7bc5b4a9fc3dc55b
Elite <shamimkhondokar0@gmail.com>
2024-03-17 13:56:52 +0600

    hff

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n1 = scanner.nextInt();
+        int n2 = scanner.nextInt();
+        int sum = n1 + n2;
+        System.out.printf("Sum = %d",sum);
+    }
+}
